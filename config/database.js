9db80f6d5181bcd2f3f7c9011eacaebd5c99686e
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-07-18 15:14:14 +0530

    Fixing vercel

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
+require('pg');
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
